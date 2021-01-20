a0d91c1c2f7de13799eebc2d21698e10fe2eb5c4
Aishwarya Dhuri <aishwaryadhuri456@gmail.com>
2021-01-20 18:59:44 +0530

    adding code for the app

---
+++ precache-manifest.7680688fd651cfe43dbebb5d502f9303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15b5f7b9f41a26955577518fb4d285f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6685322a051daff19a0a",
+    "url": "/static/css/main.3a937d0f.chunk.css"
+  },
+  {
+    "revision": "99f1a9fa987ffab2f79d",
+    "url": "/static/js/2.57b3b33d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.57b3b33d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6685322a051daff19a0a",
+    "url": "/static/js/main.6144c4b9.chunk.js"
+  },
+  {
+    "revision": "88b8069d5c7ffa7f1e7b",
+    "url": "/static/js/runtime-main.98f793fb.js"
+  }
+]);
